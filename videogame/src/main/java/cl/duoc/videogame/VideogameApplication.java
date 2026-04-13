67d5f03897df173e3bd4548bd21b551632f28c97
SJ- Alumno <SJ-Alumno@duoc.acad>
2026-04-13 11:02:16 -0400

    commit 1

---
+++ videogame/src/main/java/cl/duoc/videogame/VideogameApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.videogame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideogameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideogameApplication.class, args);
+	}
+
+}
---
+++ videogame/src/test/java/cl/duoc/videogame/VideogameApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.videogame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideogameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
